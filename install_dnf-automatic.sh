7450a033d0d58044cce632239c97891544ec71df
JeremyWaltenspuehl <101863751+JeremyWaltenspuehl@users.noreply.github.com>
2024-02-09 10:44:28 +0100

    Add files via upload

---
+++ install_dnf-automatic.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+dnf install dnf-automatic -y
+sed -i 's/reboot = never/reboot = when-needed/' /etc/dnf/automatic.conf
+sed -i 's/apply_updates = no/apply_updates = yes/' /etc/dnf/automatic.conf
+sed -i 's/emit_via = stdio/emit_via = email/' /etc/dnf/automatic.conf
+sed -i 's/email_from = root@example.com/email_from = sysacc@itcompro.com/' /etc/dnf/automatic.conf
+sed -i 's/email_to = root/email_to = support@itcompro.com/' /etc/dnf/automatic.conf
+sed -i 's/email_host = localhost/email_host = 172.16.10.24/' /etc/dnf/automatic.conf
+systemctl enable dnf-automatic.timer && systemctl start dnf-automatic.timer
+dnf-automatic
